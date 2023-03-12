cdada9f47a49b7a169af364e653ff5be2bc6040a
CWhite2127 <cwhitewesterly2@gmail.com>
2023-03-12 14:40:21 -0600

    sets up the server

--- server.js
+++ server.js
@@ -1,3 +1,18 @@
+const express = require('express');
+const path = require('path');
+const routes = require('./routes/index.js')
+
+
+const PORT = 3001;
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use('/routes', routes);
+
+
+app.use(express.static('public'));
 
 
 
@@ -8,6 +23,11 @@
 
 
 
+
+
+app.listen(PORT, () =>
+    console.log(`App listening at http://localhost:${PORT} 🚀`)
+);
 
 
 
